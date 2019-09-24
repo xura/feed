ec18441971bde3090b52bd2c7b4d55bd6fdb12cf
Joe Badaczewski <joe307bad@gmail.com>
2019-09-24 13:23:08 -0400

    simple emporium -> feed example

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import elmWebComponents from '@teamthread/elm-web-components';
 import Feed from '../elm/src/Hello.elm';
-
+import emporium from '@xura/emporium';
+console.log(emporium);
 elmWebComponents.configure('0.19');
 elmWebComponents.register('feed-div', Feed.Elm.PortExamples, {
     setupPorts: ports => {
-        let count = 0;
-        ports.receiveData.send(count.toString());
-        setInterval(() => {
-            count++;
-            ports.receiveData.send(count.toString());
-        }, 1000)
+        // let count = 0;
+        // ports.receiveData.send(count.toString());
+        //
+        //erg
+        //
+        // setInterval(() => {
+        //     count++;
+        //     ports.receiveData.send(count.toString());
+        // }, 1000)
+        emporium.count.subscribe(_ => ports.receiveData.send(_.toString()));
     },
 });
