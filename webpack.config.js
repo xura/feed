fef478a1330d48e1c94e0aa0b1cb79280e35320e
Joe Badaczewski <joe307bad@gmail.com>
2019-09-25 09:13:00 -0400

    ignoring emporium from webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './src/index.js',
@@ -18,5 +19,7 @@ module.exports = {
             }
         }]
     },
-
+    plugins: [
+        new webpack.IgnorePlugin(/@xura\/emporium/)
+    ]
 };
