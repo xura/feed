ea0e2badad3811e414d8e2e165ed92bd3d0c766c
Joe Badaczewski <joe307bad@gmail.com>
2020-01-23 13:24:05 -0500

    correcting implementation of achievements.repo.stream

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import elmWebComponents from '@teamthread/elm-web-components';
 import Feed from './elm/src/Hello.elm';
 import { connect, data, connection } from '@xura/data';
 
-(async function () {
-
-    elmWebComponents.configure('0.19');
-    elmWebComponents.register('feed-div', Feed.Elm.PortExamples, {
-        setupPorts: ports => {
-            data.achievements.stream()
-                .then(stream => stream.subscribe(achievement => {
-                    ports.receiveData.send(achievement.name.toString());
-                }));
-        },
-    });
-})();
+elmWebComponents.configure('0.19');
+elmWebComponents.register('feed-div', Feed.Elm.PortExamples, {
+    setupPorts: ports => {
+        data.achievements.repo.stream()
+            .then(stream => stream.subscribe(achievement => {
+                ports.receiveData.send(achievement.name.toString());
+            }));
+    },
+});
